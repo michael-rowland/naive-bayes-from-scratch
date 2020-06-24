2737f2270f47df5f18abf9d1aa817b7b9bb6f323
Michael Rowland <mrowland13@gmail.com>
2020-06-23 21:03:29 -0700

    initial setup

---
+++ naive_bayes.py
@@ -0,0 +1,10 @@
+
+class NaiveBayes:
+    def __init__(self):
+        # any/all setup inputs go here
+        pass
+
+    def fit(self, data):
+        # assume gaussian distribution on variables
+        # returns nothing
+        pass
